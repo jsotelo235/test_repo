820dcd4f8940f3758a1ad69be01153a5befcbcea
Jose Sotelo <jsotelo235@gmail.com>
2023-01-16 21:15:31 -0800

    init commit

---
+++ test_code/test.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+static int start_test(int argc, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+    if( argc == 2 ) {
+        
+        if(start_test(argc, argv) == 1){
+            printf("Hello World\n");
+        }
+
+    }
+    else if( argc > 2 ) {
+        printf("Too many arguments supplied.\n");
+    }
+    else {
+        printf("One argument expected.\n");
+    }
+    
+    return 0;
+}
+
+int start_test(int argc, char *argv[])
+{
+    printf("Argc: %d\n", argc);
+    printf("The argument supplied is %s\n", argv[1]);
+
+    return 1;
+}
