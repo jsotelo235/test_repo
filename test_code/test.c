4cce9f0938935a27771955242f59c806957d1737
Jose Sotelo <jsotelo235@gmail.com>
2023-01-16 21:28:05 -0800

    Testing git rebase

--- test_code/test.c
+++ test_code/test.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
+
+typedef enum test_e{
+    One,
+    Two,
+    Three,
+    Four,
+    Five
+}test_t;
+
 static int start_test(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
 {
+    int nums[] = {One, Two, Three, Four, Five};
+
     if( argc == 2 ) {
         
         if(start_test(argc, argv) == 1){
-            printf("Hello World\n");
+            printf("Hello World!\n");
         }
 
     }
