fef69b938428b0cfa5b23c259bd8633bed20f6d9
Jose Sotelo <jsotelo235@gmail.com>
2023-01-19 23:42:19 -0800

    Fixed conflicts

--- test_code/test.c
+++ test_code/test.c
@@ -11,6 +11,25 @@ const char *const test_to_str[] = {
     [Five] = "Five"
 };
 
+
+typedef struct week_t{
+
+    char *monday;
+    char *tuesday;
+    char *wednesday;
+    char *thursday;
+    char *friday;
+
+}week_t;
+
+week_t day = {.monday    = "Monday",
+              .tuesday   = "Tuesday",
+              .wednesday = "Wednesday",
+              .thursday  = "Thursday",
+              .friday    = "Friday"}; 
+
+
+
 int main(int argc, char *argv[])
 {
     test_t test_nums;
