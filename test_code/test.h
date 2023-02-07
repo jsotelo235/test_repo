f4513fdbef820e3afe2c29e32f65fd354b5cd3eb
Jose Sotelo <jsotelo235@gmail.com>
2023-02-06 19:17:55 -0800

    Fixed test.h merged issues

--- test_code/test.h
+++ test_code/test.h
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-typedef enum test_e{
+typedef enum test_e
+{
     One,
     Two,
     Three,
     Four,
     Five
-}test_t;
+} test_t;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 extern const char *const test_to_str[];
-=======
->>>>>>> 8baf908 (Added header file)
-=======
-extern const char *const test_to_str[];
->>>>>>> 187e4e0 (Testing different c concepts)
 static int start_test(int argc, char *argv[]);
